276f93ab7de347236444d1a70157d6cd2eda5b0b
Mahavir Sunil Kathed <128558230+MahavirK1997@users.noreply.github.com>
2023-12-12 19:23:24 -0500

    Update retraining.py

--- dags/src/airflow-dag/retraining.py
+++ dags/src/airflow-dag/retraining.py
@@ -2,16 +2,14 @@ import datetime as dt
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
-#LOCAL_PREPROCESS_FILE_PATH = '/tmp/data_preprocess.py'
-#GITHUB_PREPROCESS_RAW_URL = 'https://github.com/MahavirK1997/Timeseries_Lab/blob/master/src/data_preprocess.py'  # Adjust the path accordingly
+LOCAL_PREPROCESS_FILE_PATH = '/tmp/data_preprocess.py'
+GITHUB_PREPROCESS_RAW_URL = 'https://raw.githubusercontent.com/MahavirK1997/Timeseries_Lab/master/src/data_preprocess.py'
 
 LOCAL_TRAIN_FILE_PATH = '/tmp/train.py'
-GITHUB_TRAIN_RAW_URL = 'https://github.com/pavit14/Early-Prediction-of-Sepsis/blob/mahavir_mlops/dags/src/trainer/train.py'  # Adjust the path accordingly
-
-
+GITHUB_TRAIN_RAW_URL = 'https://raw.githubusercontent.com/MahavirK1997/Timeseries_Lab/master/src/trainer/train.py'
 
 default_args = {
-    'owner': 'Sepsis_project',
+    'owner': 'Time_Series_IE7374',
     'start_date': dt.datetime(2023, 10, 24),
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=5),
@@ -20,11 +18,17 @@ default_args = {
 dag = DAG(
     'model_retraining',
     default_args=default_args,
-    description='Model retraining at 9 PM everyday',
+    description='Model retraining at 9 PM every day',
     schedule_interval='0 21 * * *',  # Every day at 9 pm
     catchup=False,
 )
 
+# Tasks for pulling scripts from GitHub
+pull_preprocess_script = BashOperator(
+    task_id='pull_preprocess_script',
+    bash_command=f'curl -o {LOCAL_PREPROCESS_FILE_PATH} {GITHUB_PREPROCESS_RAW_URL}',
+    dag=dag,
+)
 
 pull_train_script = BashOperator(
     task_id='pull_train_script',
@@ -32,12 +36,24 @@ pull_train_script = BashOperator(
     dag=dag,
 )
 
+# Add print statements for debugging
+print_local_train_file = BashOperator(
+    task_id='print_local_train_file',
+    bash_command=f'ls -l {LOCAL_TRAIN_FILE_PATH}',
+    dag=dag,
+)
 
 env = {
-    'AIP_STORAGE_URI': 'gs://sepsis_pred_bucket/model'
+    'AIP_STORAGE_URI': 'gs://timeseries_data_model_bucket/model'
 }
 
-
+# Tasks for running scripts
+run_preprocess_script = BashOperator(
+    task_id='run_preprocess_script',
+    bash_command=f'python {LOCAL_PREPROCESS_FILE_PATH}',
+    env=env,
+    dag=dag,
+)
 
 run_train_script = BashOperator(
     task_id='run_train_script',
@@ -47,5 +63,4 @@ run_train_script = BashOperator(
 )
 
 # Setting up dependencies
-pull_train_script >> run_train_script
-
+pull_preprocess_script >> pull_train_script >> print_local_train_file >> run_preprocess_script >> run_train_script
